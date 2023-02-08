1b90c4aa83891849cf1fe3384d4b6b7211e75228
npaudyal321 <naween321@gmail.com>
2023-02-08 12:13:46 +0545

    [add] adds model registration

--- heart/admin.py
+++ heart/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bus, Route, BusRoute
 
-# Register your models here.
+admin.site.register(Bus)
+admin.site.register(Route)
+admin.site.register(BusRoute)
